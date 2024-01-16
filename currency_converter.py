78b28ef643964f678c8c8914448c2cabd81e8c5b
Najeeb Ahmad (Web Developer) <129047186+Najeebahmad07@users.noreply.github.com>
2024-01-16 10:45:13 +0530

    Add files via upload

---
+++ currency_converter.py
@@ -0,0 +1,6 @@
+a=float(input("Enter Us Dollar "))
+b=a*82.96
+print("----------------------------------")
+print("Successfully Convert To Indian Ruppees")
+print("----------------------------------")
+print(b)
