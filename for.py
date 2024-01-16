56ba10a4667403911c3cb3be7aa05d167663edb2
Najeeb Ahmad (Web Developer) <129047186+Najeebahmad07@users.noreply.github.com>
2024-01-16 10:38:39 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,7 @@
+'''for a in range(1,100):
+    print(a)'''
+
+a=int(input("Enter A Number"))
+
+for x in range(1,11):
+    print(x*a)
---
+++ nested.py
@@ -0,0 +1,6 @@
+a=["A","B","c","D","E"]
+b=[1,2,3,4,5]
+
+for x in a:
+    for y in b:
+        print(x,y)
